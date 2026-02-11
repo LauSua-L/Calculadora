f0230f0f0cd893a183e488a63002c40f8784966d
karen <lopdofi12345@gmail.com>
2026-02-10 23:45:24 -0500

    se agregaron las tres funciones

--- script.js
+++ script.js
@@ -4,12 +4,15 @@ const pantalla = document.getElementById('pantalla');
 function agregar(valor){
     pantalla.value += valor;
 }
+
 function limpiar(){
     pantalla.value='';
 }
+
 function borrarUno(){
     pantalla.value = pantalla.value.slice(0,-1)
 }
+
 function calcular(){
 try{
     pantalla.value = eval(pantalla.value);
@@ -18,4 +21,18 @@ try{
     pantalla.value='error';
     setTimeout(() => limpiar(), 1500)
 }
+}
+
+function calcularRaiz() {
+    // Math.sqrt calcula la raíz del número que esté en la pantalla
+    pantalla.value = Math.sqrt(eval(pantalla.value));
+}
+
+function calcularPotencia() {
+    const numero = eval(pantalla.value);
+    pantalla.value = Math.pow(numero, 2); // Eleva al cuadrado
+}
+
+function calcularPorcentaje() {
+    pantalla.value = eval(pantalla.value) / 100;
 }
