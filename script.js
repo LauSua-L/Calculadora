fef333bfcb71e7fe45ada1e8504c5b550f7735da
karen <lopdofi12345@gmail.com>
2026-02-10 23:58:04 -0500

    se agrego la funcion de el teclado

--- script.js
+++ script.js
@@ -36,3 +36,41 @@ function calcularPotencia() {
 function calcularPorcentaje() {
     pantalla.value = eval(pantalla.value) / 100;
 }
+
+
+// esta funcion se crea ahora el teclado y las teclas tienen las mismas funciones
+function escribir(valor) {
+    pantalla.value += valor;
+}
+
+// se escucha al teclado
+document.addEventListener('keydown', function(event) {
+    const tecla = event.key;
+
+    // Si es un número del 0 al 9
+    if (tecla >= '0' && tecla <= '9') {
+        escribir(tecla); 
+    }
+
+    // Si son operadores
+    if (['+', '-', '*', '/'].includes(tecla)) {
+        escribir(tecla);
+    }
+
+    // teclas especiales
+    if (tecla === 'Enter') {
+        event.preventDefault(); // Para que no haga cosas raras por su funcion por defecto
+        calcular(); //esta queda igual
+    }
+
+    if (tecla === 'Backspace') {
+        borrarUno(); //  borrar
+    }
+
+    if (tecla === 'Escape') {
+        limpiarPantalla(); // función de la C
+    }
+    if (tecla === '%') {
+    calcularPorcentaje(); 
+}
+});
